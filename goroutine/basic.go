dcdf6fc7da7a798e9b06434d23a1c41744c9a14e
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-10-17 21:07:51 -0300

    goroutine basic

---
+++ goroutine/basic.go
@@ -0,0 +1,25 @@
+package goroutine
+
+import (
+	"fmt"
+	"time"
+)
+
+func init() {
+	go load()
+	countUntil(5)
+}
+
+func load() {
+	for {
+		fmt.Println("...")
+		time.Sleep(time.Millisecond * 600)
+	}
+}
+
+func countUntil(limit int) {
+	for i := 0; i < limit; i++ {
+		time.Sleep(time.Second * 1)
+	}
+	fmt.Println(limit)
+}
